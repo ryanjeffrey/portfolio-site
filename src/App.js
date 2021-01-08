dd2eb7943bcec715fad98f7fb3f0c58c6b43466f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-01-08 17:05:14 -0600

    Deleted Articles pages, routed paths in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import logo from './logo.svg';
 // Import the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom'; 
 import Projects from './Projects.js'; 
-import Articles from './Articles.js'; 
 import About from './About.js'; 
 import './App.css';
  
@@ -14,18 +13,15 @@ function App() {
     <BrowserRouter>
       <div className="App">
   
-        // Set up the Router
-        <Route exact path="/" component={Projects} />
-        <Route path="/articles" component={Articles} />
+
+        <Route exact path="/projects" component={Projects} />
         <Route path="/about" component={About} />
   
         <div className="navigation">
           <img src={logo} className="logo-image" alt="Logo Image" />
           <div className="navigation-sub">
                                           
-            // Set up the Links
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/projects" className="item">Projects</Link>
             <Link to="/about" className="item">About</Link>
   
           </div>
